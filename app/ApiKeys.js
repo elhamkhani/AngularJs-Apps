e88cb1fd3642bca02cd5d04dc1b00ddc86ca7131
elham <elham.khani@gmail.com>
2016-01-17 00:59:42 +0000

    stop tracking APIkeys

--- app/ApiKeys.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Elham.Khani on 12/01/2016.
- */
-var config = {
-     googleKey : 'AIzaSyCcAd_LTLqtzoCqTHShUxNjGLPxfCsTABo',
-     nasaKey : 'Erj1DXpDoYLc8yl2bNOILUbprBGKQBLhKSo7BlRn',
-};
